8bf2af0cedfb2bacac088d3083b2dbc4499209e4
ricardo <ricardor3000@gmail.com>
2026-03-24 13:10:26 -0300

    services

---
+++ src/app/(inner-page)/service-details/[slug]/page.jsx
@@ -0,0 +1,126 @@
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import HeaderOne from "@/components/header/HeaderOne";
+import BackToTop from "@/components/BackToTop";
+import Breadcrumb from "@/components/Breadcrumb";
+import FooterOne from "@/components/footer/FooterOne";
+import Services from "@/data/Services.json";
+
+export async function generateStaticParams() {
+  return Services.map((service) => ({
+    slug: service.slug,
+  }));
+}
+
+export default async function ServiceDetailsPage({ params }) {
+  const { slug } = await params;
+  const service = Services.find((item) => item.slug === slug);
+
+  if (!service) {
+    notFound();
+  }
+
+  const breadcrumbs = [
+    { label: "Inicio", link: "/" },
+    { label: "Nuestros Servicios", link: "/#service" },
+    { label: service.title },
+  ];
+
+  const otherServices = Services.filter((item) => item.slug !== service.slug);
+
+  return (
+    <div>
+      <HeaderOne />
+      <Breadcrumb title={service.title} breadcrumbs={breadcrumbs} />
+
+      <div className="rts-service-details-area rts-section-gap">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-8 col-md-12 col-sm-12 col-12">
+              <div className="service-detials-step-1">
+                <div className="thumbnail">
+                  <img
+                    src={`/assets/images/service/${service.image}`}
+                    alt={service.title}
+                  />
+                </div>
+              </div>
+
+              {service.sections.map((section, index) => (
+                <div
+                  key={index}
+                  className={`service-detials-step-2 ${index > 0 ? "mt--40" : "mt--30"}`}
+                >
+                  <h4 className="title">{section.title}</h4>
+
+                  {section.paragraphs.map((paragraph, pIndex) => (
+                    <p
+                      key={pIndex}
+                      className={`disc ${pIndex === 0 ? "mb--25" : ""}`}
+                    >
+                      {paragraph}
+                    </p>
+                  ))}
+                </div>
+              ))}
+            </div>
+
+            <div className="col-xl-4 col-md-12 col-sm-12 col-12 mt_lg--60 pl--50 pl_md--0 pl-lg-controler pl_sm--0">
+              <div className="rts-single-wized Categories service">
+                <div className="wized-header">
+                  <h5 className="title">Categoría</h5>
+                </div>
+                <div className="wized-body">
+                  <ul className="single-categories">
+                    <li>
+                      <Link href="#">
+                        {service.category} <i className="far fa-long-arrow-right" />
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="rts-single-wized Categories service mt--30">
+                <div className="wized-header">
+                  <h5 className="title">Otros Servicios</h5>
+                </div>
+                <div className="wized-body">
+                  {otherServices.map((item) => (
+                    <ul className="single-categories" key={item.id}>
+                      <li>
+                        <Link href={`/service-details/${item.slug}`}>
+                          {item.title} <i className="far fa-long-arrow-right" />
+                        </Link>
+                      </li>
+                    </ul>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rts-single-wized contact service mt--30">
+                <div className="wized-header">
+                  <Link href="/">
+                    <img
+                      src="/assets/images/logo/logo-2.svg"
+                      alt="Business_logo"
+                    />
+                  </Link>
+                </div>
+                <div className="wized-body">
+                  <h5 className="title">Need Help? We Are Here To Help You</h5>
+                  <Link className="rts-btn btn-primary" href="/contact">
+                    Contact Us
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <FooterOne />
+      <BackToTop />
+    </div>
+  );
+}
--- src/components/service/ServiceOne.jsx
+++ src/components/service/ServiceOne.jsx
@@ -1,224 +1,95 @@
-"use client"
-import React from 'react'
-import Link from 'next/link';
-function ServiceOne() {
-    return (
-        <div>
-            <>
-                {/* rts service post area  Start*/}
-                <div className="rts-service-area rts-section-gapBottom" id="service">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="rts-title-area service text-center">
-                                    <p className="pre-title">LapDev Servicios</p>
-                                    <h2 className="title">Nuestros Servicios</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="container-fluid service-main plr--120-service mt--50 plr_md--0 pl_sm--0 pr_sm--0">
-                        <div className="background-service row">
-                            {/* start single Service */}
-                            <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div className="service-one-inner one">
-                                    <div className="thumbnail">
-                                        <img
-                                            src="assets/images/service/icon/01.svg"
-                                            alt="finbiz_service"
-                                        />
-                                    </div>
-                                    <div className="service-details">
-                                        <Link href={'/service-details'}>
-                                            <h5 className="title">Desarrollo Web (Next.js/React)</h5>
-                                        </Link>
-                                        <p className="disc">
-                                            Creamos aplicaciones web modernas, rápidas y escalables utilizando Next.js y React. 
-                                            Desarrollamos experiencias optimizadas para rendimiento, SEO y usabilidad, con 
-                                            arquitecturas limpias y mantenibles.
-                                        </p>
-                                        <Link
-                                            className="rts-read-more btn-primary"
-                                            href={'/service-details'}
-                                        >
-                                            <i className="far fa-arrow-right" />
-                                           Más Información
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* end single Services */}
-                            {/* start single Service */}
-                            <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div className="service-one-inner two">
-                                    <div className="thumbnail">
-                                        <img
-                                            src="assets/images/service/icon/02.svg"
-                                            alt="finbiz_service"
-                                        />
-                                    </div>
-                                    <div className="service-details">
-                                        <Link href={'/service-details'}>
-                                            <h5 className="title">Backend & APIs</h5>
-                                        </Link>
-                                        <p className="disc">
-                                            Diseñamos y desarrollamos backends robustos y APIs seguras que garantizan rendimiento,
-                                            escalabilidad e integración eficiente con otros sistemas. Trabajamos con arquitecturas modernas 
-                                            y bases de datos optimizadas.
-                                        </p>
-                                        <Link
-                                            className="rts-read-more btn-primary"
-                                            href={'/service-details'}
-                                        >
-                                            <i className="far fa-arrow-right" />
-                                            Más Información
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* end single Services */}
-                            {/* start single Service */}
-                            <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div className="service-one-inner three">
-                                    <div className="thumbnail">
-                                        <img
-                                            src="assets/images/service/icon/03.svg"
-                                            alt="finbiz_service"
-                                        />
-                                    </div>
-                                    <div className="service-details">
-                                        <Link href={'/service-details'}>
-                                            <h5 className="title">Apps Mobile</h5>
-                                        </Link>
-                                        <p className="disc">
-                                            Construimos aplicaciones móviles nativas o multiplataforma con enfoque en rendimiento, experiencia de usuario y estabilidad. 
-                                            Desde el diseño hasta la publicación en stores, acompañamos todo el proceso.
-                                        </p>
-                                        <Link
-                                            className="rts-read-more btn-primary"
-                                            href={'/service-details'}
-                                        >
-                                            <i className="far fa-arrow-right" />
-                                            Más Información
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* end single Services */}
-                            {/* start single Service */}
-                            <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div className="service-one-inner four">
-                                    <div className="thumbnail">
-                                        <img
-                                            src="assets/images/service/icon/04.svg"
-                                            alt="finbiz_service"
-                                        />
-                                    </div>
-                                    <div className="service-details">
-                                        <Link href={'/service-details'}>
-                                            <h5 className="title">UX/UI & Product Design</h5>
-                                        </Link>
-                                        <p className="disc">
-                                            Diseñamos productos digitales centrados en el usuario, combinando investigación, estrategia y diseño visual. 
-                                            Creamos experiencias intuitivas que alinean objetivos de negocio con necesidades reales de los usuarios.
-                                        </p>
-                                        <Link
-                                            className="rts-read-more btn-primary"
-                                            href={'/service-details'}
-                                        >
-                                            <i className="far fa-arrow-right" />
-                                            Más Información
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* end single Services */}
-                            {/* start single Service */}
-                            <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div className="service-one-inner five">
-                                    <div className="thumbnail">
-                                        <img
-                                            src="assets/images/service/icon/05.svg"
-                                            alt="finbiz_service"
-                                        />
-                                    </div>
-                                    <div className="service-details">
-                                        <Link href={'/service-details'}>
-                                            <h5 className="title">DevOps/Cloud</h5>
-                                        </Link>
-                                        <p className="disc">
-                                            Implementamos infraestructuras en la nube seguras y escalables. Automatizamos procesos de despliegue, 
-                                            integración continua y monitoreo para garantizar eficiencia, estabilidad y crecimiento sostenible.
-                                        </p>
-                                        <Link
-                                            className="rts-read-more btn-primary"
-                                            href={'/service-details'}
-                                        >
-                                            <i className="far fa-arrow-right" />
-                                            Más Información
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* end single Services */}
-                            {/* start single Service */}
-                            <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div className="service-one-inner six">
-                                    <div className="thumbnail">
-                                        <img
-                                            src="assets/images/service/icon/06.svg"
-                                            alt="finbiz_service"
-                                        />
-                                    </div>
-                                    <div className="service-details">
-                                        <Link href={'/service-details'}>
-                                            <h5 className="title">Mantenimiento / Soporte / Evolutivos</h5>
-                                        </Link>
-                                        <p className="disc">
-                                            Ofrecemos soporte continuo, mejoras evolutivas y optimización constante de sistemas existentes. Aseguramos estabilidad, 
-                                            actualización tecnológica y adaptación a nuevas necesidades del negocio.
-                                        </p>
-                                        <Link
-                                            className="rts-read-more btn-primary"
-                                            href={'/service-details'}
-                                        >
-                                            <i className="far fa-arrow-right" />
-                                            Más Información
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* end single Services */}
-                        </div>
-                        <div className="row">
-                             <div
-                               className="cta-one-bg col-12"
-                               style={{
-                                 borderRadius: "20px",
-                                 overflow: "hidden"
-                            }}
-                             >
-                            <div className="cta-one-inner">
-                                <div className="cta-left">
-                                  <h3 className="title">
-                                     Creamos Soluciones Digitales que Transforman Negocios
-                                  </h3>
-                                </div>
-                                 <div className="cta-right">
-                                   <Link className="rts-btn btn-white" href="appoinment.html">
-                                     Contáctanos
-                                   </Link>
-                                </div>
-                               </div>
-                             </div>
-                        </div>
-                    </div>
-                </div>
-                {/* rts service post area ENd */}
-            </>
+"use client";
 
+import React from "react";
+import Link from "next/link";
+import Services from "@/data/Services.json";
+
+function ServiceOne() {
+  const serviceClasses = ["one", "two", "three", "four", "five", "six"];
+
+  return (
+    <div>
+      <>
+        {/* rts service post area Start */}
+        <div className="rts-service-area rts-section-gapBottom" id="service">
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <div className="rts-title-area service text-center">
+                  <p className="pre-title">LapDev Servicios</p>
+                  <h2 className="title">Nuestros Servicios</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="container-fluid service-main plr--120-service mt--50 plr_md--0 pl_sm--0 pr_sm--0">
+            <div className="background-service row">
+              {Services.map((service, index) => (
+                <div
+                  key={service.id}
+                  className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12"
+                >
+                  <div
+                    className={`service-one-inner ${
+                      serviceClasses[index] || "one"
+                    }`}
+                  >
+                    <div className="thumbnail">
+                      <img
+                        src={`/assets/images/service/icon/${service.icon}`}
+                        alt={service.title}
+                      />
+                    </div>
+
+                    <div className="service-details">
+                      <Link href={`/service-details/${service.slug}`}>
+                        <h5 className="title">{service.title}</h5>
+                      </Link>
+
+                      <p className="disc">{service.summary}</p>
+
+                      <Link
+                        className="rts-read-more btn-primary"
+                        href={`/service-details/${service.slug}`}
+                      >
+                        <i className="far fa-arrow-right" />
+                        Más Información
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div className="row">
+              <div
+                className="cta-one-bg col-12"
+                style={{
+                  borderRadius: "20px",
+                  overflow: "hidden",
+                }}
+              >
+                <div className="cta-one-inner">
+                  <div className="cta-left">
+                    <h3 className="title">
+                      Creamos Soluciones Digitales que Transforman Negocios
+                    </h3>
+                  </div>
+                  <div className="cta-right">
+                    <Link className="rts-btn btn-white" href="/contact">
+                      Contáctanos
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+        {/* rts service post area End */}
+      </>
+    </div>
+  );
 }
 
-export default ServiceOne
+export default ServiceOne;
