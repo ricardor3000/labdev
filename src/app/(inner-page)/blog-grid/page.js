f6ae459393c51590a1e4c3fd1c9c24a0035da97d
ricardo <ricardor3000@gmail.com>
2026-04-14 16:38:16 -0400

    eliminarBarraBlog

--- src/app/(inner-page)/blog-grid/page.js
+++ src/app/(inner-page)/blog-grid/page.js
@@ -19,7 +19,7 @@ export default function Home() {
         <div className="">
             <HeaderOne />
 
-            <Breadcrumb title="Blog Lapdev" breadcrumbs={breadcrumbs} />
+           {/*  <Breadcrumb title="Blog Lapdev" breadcrumbs={breadcrumbs} /> */}
 
             <BackToTop />
 
